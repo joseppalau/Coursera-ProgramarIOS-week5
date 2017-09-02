c8fd25ba8c56fd2428ee2eef2679d6e6d07b1b90
Administrador <administrador@iMac-de-Administrador.local>
2017-09-02 15:59:27 +0200

    Initial Commit

---
+++ mini-reto-week5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mini-reto-week5
+//
+//  Created by Administrador on 2/9/17.
+//  Copyright © 2017 palauturf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
